9b74135601b6f3eaba73d377a1d2f2beea2f8ab4
Christopher Jelly <christopher.p.jelly@gmail.com>
2026-05-23 11:26:52 -0400

    fix(aws): allow heartbeat apply tagging reads

--- infrastructure/aws/identity/main.tf
+++ infrastructure/aws/identity/main.tf
@@ -284,6 +284,7 @@ data "aws_iam_policy_document" "main" {
       "budgets:CreateSubscriber",
       "budgets:DeleteSubscriber",
       "budgets:DescribeSubscribersForNotification",
+      "budgets:TagResource",
       "budgets:UpdateSubscriber",
     ]
     resources = ["*"]
@@ -392,11 +393,18 @@ data "aws_iam_policy_document" "main" {
   }
 
   statement {
+    sid = "CloudWatchLogsList"
+    actions = [
+      "logs:DescribeLogGroups",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
     sid = "CloudWatchLogsHeartbeat"
     actions = [
       "logs:CreateLogGroup",
       "logs:DeleteLogGroup",
-      "logs:DescribeLogGroups",
       "logs:ListTagsForResource",
       "logs:PutRetentionPolicy",
       "logs:TagResource",
